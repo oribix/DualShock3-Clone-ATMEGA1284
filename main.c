5a8d52f9e0f5d1eb4e8dfff7d623a47877c3f3e2
Marco Morelos <Marco Morelos>
2016-11-20 02:37:50 -0800

    contstructed most of controller vector

--- main.c
+++ main.c
@@ -9,27 +9,56 @@
 #include <avr/eeprom.h> 
 #include <avr/portpins.h> 
 #include <avr/pgmspace.h> 
-
-//bit functions 
-#include "bit.h"
-
-//drivers
-#include "A2D.h"
-#include "accelerometer.h"
-#include "usart_ATmega1284.h"
  
 //FreeRTOS include files 
 #include "FreeRTOS.h" 
 #include "task.h" 
 #include "croutine.h"
 
+//binary manipulation helpers
+#include "bit.h"
+//drivers
+#include "A2D.h"
+#include "accelerometer.h"
+#include "usart_ATmega1284.h"
 
-#define TICKRATE 20
+#define TICKRATE 20         //ticks per second
+#define TS_PRECISION  6     //thumb stick bit precision
+#define TILT_PRECISION 8    //accelerometer tilt precision
+
+//thumb stick pin numbers
+#define RIGHTH  0
+#define RIGHTV  1
+#define LEFTH   2
+#define LEFTV   3
+
+long long shiftIn(unsigned long long cv, unsigned char val, unsigned char n){
+    cv <<= n;
+    cv |= val;
+    return cv;
+}
+
+//returns ADC value of thumb stick at pinNum
+unsigned char getThumbStickVal(unsigned char pinNum){
+    Set_A2D_Pin(pinNum);
+    unsigned char tsTruncate = ADC_PRECISION - TS_PRECISION;
+    static unsigned char i;
+    for(i = 0; i < 24; i++) asm("nop");
+    unsigned char thumbStickVal = ADC >> tsTruncate;
+    return thumbStickVal;
+}
+
+//inverts thumb stick
+unsigned char invertThumbStick(unsigned char ts){
+    unsigned char maxValTS = (1 << TS_PRECISION) - 1;
+    return maxValTS - ts;
+}
 
 enum ControllerState {controller_INIT, controller_WAIT} controllerState;
-signed char xTilt, yTilt, zTilt;
-unsigned char rightH, rightV, leftH, leftV;
-unsigned long long controllerVector;
+signed char xTilt, yTilt, zTilt;                //accelerometer values
+unsigned char rightH, rightV, leftH, leftV;     //thumb stick values
+unsigned short buttons;                         //button vector
+unsigned long long controllerVector;            //64bit data vector holding all the measured values
 
 void controllerInit(){
     controllerState = controller_INIT;
@@ -39,31 +68,47 @@ void controllerTick(){
     //Actions
     switch(controllerState){
         case controller_INIT:
-        controllerVector = 0;
-        xTilt = 0;
-        yTilt = 0;
-        zTilt = 0;
+            xTilt = 0;
+            yTilt = 0;
+            zTilt = 0;
+            rightH = 0;
+            rightV = 0;
+            leftH = 0;
+            leftV = 0;
+            buttons = 0;
+            controllerVector = 0;
         break;
         
         case controller_WAIT:
-        //get accellerometer values
+        //get accelerometer values
         xTilt = accReadAddress(LIS3DH_REG_OUT_X_H);
         yTilt = accReadAddress(LIS3DH_REG_OUT_Y_H);
         zTilt = accReadAddress(LIS3DH_REG_OUT_Z_H);
         
         //get thumb stick values
-        Set_A2D_Pin(0); rightH = ADC >> 2;
-        Set_A2D_Pin(1); rightV = ADC >> 2;
-        Set_A2D_Pin(2); leftH  = ADC >> 2;
-        Set_A2D_Pin(3); leftV  = ADC >> 2;
+        rightH = getThumbStickVal(RIGHTH);
+        rightV = getThumbStickVal(RIGHTV);
+        leftH  = getThumbStickVal(LEFTH);
+        leftV  = getThumbStickVal(LEFTV);
         
-        //wait for USART to be ready
+        //invert horizontal axis
+        //I do this because of the way the sticks are wired on the breadboard
+        rightH = invertThumbStick(rightH);
+        leftH  = invertThumbStick(leftH);
+        
+        //construct controller vector
+        controllerVector = shiftIn(controllerVector, xTilt, TILT_PRECISION);
+        controllerVector = shiftIn(controllerVector, yTilt, TILT_PRECISION);
+        controllerVector = shiftIn(controllerVector, zTilt, TILT_PRECISION);
+        controllerVector = shiftIn(controllerVector, leftV,  TS_PRECISION);
+        controllerVector = shiftIn(controllerVector, leftH,  TS_PRECISION);
+        controllerVector = shiftIn(controllerVector, rightV, TS_PRECISION);
+        controllerVector = shiftIn(controllerVector, rightH, TS_PRECISION);
+        
+        //send values
         while(!USART_IsSendReady(0));
-        //send data
-        USART_Send(rightV, 0);
-        //wait for transmission to complete
-        while(!USART_HasTransmitted(0)){}
-        //flush USART
+        USART_Send(leftH, 0);
+        while(!USART_HasTransmitted(0));
         USART_Flush(0);
 
         break;
@@ -102,6 +147,7 @@ void startController(unsigned portBASE_TYPE Priority)
 
 int main(void)
 {
+    DDRA = 0x00; PORTA = 0xFF;
     DDRC = 0xFF; PORTC = 0x00;
     
     initUSART(0);
